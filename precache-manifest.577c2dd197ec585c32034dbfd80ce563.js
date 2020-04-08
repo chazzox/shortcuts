4e26da7e00594cb087a90cec89f6c44928c7bf7d
chazzox <42206116+chazzox@users.noreply.github.com>
2020-04-08 16:48:23 +0100

    Updates

---
+++ precache-manifest.577c2dd197ec585c32034dbfd80ce563.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177fb7465921e32415b3f8b7c00cfd15",
+    "url": "/shortcuts-beta/index.html"
+  },
+  {
+    "revision": "7ed2de98b5f2235064ee",
+    "url": "/shortcuts-beta/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76880a6251976dc8b11b",
+    "url": "/shortcuts-beta/static/js/2.889aaf9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shortcuts-beta/static/js/2.889aaf9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed2de98b5f2235064ee",
+    "url": "/shortcuts-beta/static/js/main.03987a64.chunk.js"
+  },
+  {
+    "revision": "d94b424bc40378238bcd",
+    "url": "/shortcuts-beta/static/js/runtime-main.77096d07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shortcuts-beta/static/media/logo.5d5d9eef.svg"
+  }
+]);
