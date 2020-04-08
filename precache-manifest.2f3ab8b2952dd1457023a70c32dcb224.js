f71f4ca2c1c30ca8a1fc0511dc766b3e6792d1da
charlie <42206116+chazzox@users.noreply.github.com>
2020-04-08 15:43:00 +0100

    testing react compatibility

---
+++ precache-manifest.2f3ab8b2952dd1457023a70c32dcb224.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b316f0abb04c5b595f67b8ea00cbcb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84408e73863606142a77",
+    "url": "/static/css/main.e7d08bf2.chunk.css"
+  },
+  {
+    "revision": "a49cbc548ce5ac328d14",
+    "url": "/static/js/2.dc5eab89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dc5eab89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84408e73863606142a77",
+    "url": "/static/js/main.5c31f54d.chunk.js"
+  },
+  {
+    "revision": "3426785670d13b9274db",
+    "url": "/static/js/runtime-main.150f8b7e.js"
+  },
+  {
+    "revision": "888089a5498dfd4fbb08d72e4c5f2ae9",
+    "url": "/static/media/logo.888089a5.svg"
+  }
+]);
