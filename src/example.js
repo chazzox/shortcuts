91ee83c3396264ca5bf400764e9b82c027b254c6
chazzox <42206116+chazzox@users.noreply.github.com>
2020-04-09 03:39:18 +0100

    added working render function, and full example json file
    also removed intitial create react app assets

---
+++ src/example.js
@@ -0,0 +1,99 @@
+export const example = {
+			name: 'chaz',
+			darkMode: true,
+			config: [
+				[
+					{
+						boxName: 'Social',
+						type: 'links',
+						linkArr: [
+							{ name: 'Reddit', url: 'https://www.google.com' },
+							{ name: 'Steam', url: 'https://www.steam.com' },
+							{ name: 'Instagram', url: 'https://www.Instagram.com' },
+							{ name: 'Inbox', url: 'https://www.mail.google.com' },
+							{ name: 'Discord', url: 'https://www.discord.com' },
+							{ name: 'Imgur', url: 'https://www.imgur.com' },
+							{ name: 'Twitter', url: 'https://www.twitter.com' }
+						]
+					},
+					{
+						boxName: 'Updates',
+						type: 'links',
+						linkArr: [
+							{ name: 'Rust Dev Log', url: 'https://www.facepunch.com' },
+							{ name: 'PUBG News', url: 'https://www.playbattlegrounds.com' },
+							{ name: 'CS:GO Path Notes', url: 'https://www.blog.counter-strike.net/index.php/category/updates/' }
+						]
+					}
+				],
+				[
+					{
+						boxName: 'Videos',
+						type: 'links',
+						linkArr: [
+							{ name: 'Youtube', url: 'https://www.youtube.com' },
+							{ name: 'Twitch', url: 'https://www.twitch.tv' },
+							{ name: 'Netflix', url: 'https://www.netflix.com' },
+							{ name: 'Channel4', url: 'https://www.channel4.com' },
+							{ name: 'BBC iPlayer', url: 'https://www.bbc.co.uk/iplayer' }
+						]
+					},
+					{
+						boxName: 'iOS',
+						type: 'links',
+						linkArr: [
+							{ name: 'BundleID', url: 'https://www.offcornerdev.com' },
+							{ name: 'Apple Identifiers', url: 'https://pinpal.github.io/stock-identifiers/' }
+						]
+					},
+					{
+						boxName: 'Cloud',
+						type: 'links',
+						linkArr: [
+							{ name: 'Icloud', url: 'https://www.icloud.com' },
+							{ name: 'MediaFire', url: 'https://www.mediafire.com' }
+						]
+					}
+				],
+				[
+					{
+						boxName: 'Utilities',
+						type: 'links',
+						linkArr: [
+							{ name: 'Speedtest', url: 'https://www.Speedtest.com' },
+							{ name: 'FileMail', url: 'https://www.FileMail.com' },
+							{ name: 'Goo.gl', url: 'https://www.Goo.gl' },
+							{ name: 'TempMail', url: 'https://www.TempMail.com' }
+						]
+					},
+					{
+						boxName: 'Shopping',
+						type: 'links',
+						linkArr: [
+							{ name: 'ebay', url: 'https://www.ebay.com' },
+							{ name: 'Amazon', url: 'https://www.Amazon.com' },
+							{ name: 'PayPal', url: 'https://www.PayPal.com' },
+							{ name: 'LLoyds Bank', url: 'https://www.LLoydsBank.com' },
+							{ name: 'PCPartPicker', url: 'https://www.PCPartPicker.com' },
+							{ name: 'IKea', url: 'https://www.IKea.com' }
+						]
+					}
+				],
+				[
+					{
+						boxName: 'CS:GO',
+						type: 'links',
+						linkArr: [
+							{ name: 'PvPro', url: 'https://www.pvpro.com' },
+							{ name: 'Derank', url: 'https://www.derank.me' },
+							{ name: 'Workshop', url: 'https://steamcommunity.com/workshop/browse/?appid=730' }
+						]
+					},
+					{
+						boxName: 'Weather',
+						type: 'widget',
+						widgetType: 'weather'
+					}
+				]
+			]
+		};
--- src/index.js
+++ src/index.js
@@ -1,27 +1,136 @@
-import React, { Component } from 'react';
+// React Modules
+import React, { PureComponent, Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import logo from './logo.svg';
+/// ofline caching service
 import * as serviceWorker from './serviceWorker';
+/// our modules
+import './index.scss';
+import { example } from './example';
 
-class Main extends Component {
+class Main extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = { userJSON: this.props.userJSON, config: this.props.userJSON.config, columns: 'lol' };
+	}
+	renderColumns() {
+		return this.state.config.map((column, index) => {
+			return <Column key={index} index={index} column={column} />;
+		});
+	}
+	componentDidMount() {
+		if (this.state.userJSON !== undefined) {
+			this.setState({ columns: this.renderColumns() });
+		}
+	}
 	render() {
 		return (
 			<React.StrictMode>
 				<div className='App'>
-					<header className='App-header'>
-						<img src={logo} className='App-logo' alt='logo' />
-						<p>
-							are we gaming?
-						</p>
-					</header>
+					<CustomHeader />
+					<div className='wrapper'>{this.state.columns}</div>
 				</div>
-			</React.StrictMode> 
+			</React.StrictMode>
+		);
+	}
+}
+class CustomHeader extends Component {
+	render() {
+		return (
+			<div className='App-header'>
+				<h1>SHORTCUTS</h1>
+				<h1 className='yeah'>made for gamers, by gamers</h1>
+			</div>
 		);
 	}
 }
 
-ReactDOM.render(<Main/>, document.getElementById('root'));
+class Column extends Component {
+	constructor(props) {
+		console.log(props);
+		super(props);
+		this.state = { column: this.props.column, boxArr: '' };
+		// this.links = this.renderLinkList()
+	}
+	renderBox() {
+		return this.state.column.map((box, index) => {
+			return <Box key={index} box={box} />;
+		});
+	}
+	componentDidMount() {
+		this.setState({ boxArr: this.renderBox() });
+	}
+	render() {
+		// return <div><h1>yeah</h1></div>;
+		return <div className='column'>{this.state.boxArr}</div>;
+	}
+}
 
-// optimises render workflow (caching)
+class Box extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { box: this.props.box, content: '' };
+	}
+	componentDidMount() {
+		console.log(this.state.box);
+		if (this.state.box.type === 'links') {
+			this.setState({ content: this.renderLinks() });
+		} else if (this.state.box.type === 'widget') {
+			switch (this.state.box.widgetType) {
+				case 'weather':
+					this.setState({ content: <WeatherWidget /> });
+					break;
+
+				default:
+					this.setState({ content: <WeatherWidget /> });
+					break;
+			}
+		}
+	}
+	renderLinks() {
+		return this.state.box.linkArr.map((link, index) => {
+			return <Link key={index} link={link} />;
+		});
+	}
+	render() {
+		return (
+			<div>
+				<h1 className='boxName'>{this.state.box.boxName}</h1>
+				<div className='box'>{this.state.content}</div>
+			</div>
+		);
+	}
+}
+
+class WeatherWidget extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { type: this.props.type, links: '' };
+	}
+	render() {
+		return <h1 className='linkName'>weather, init mate</h1>;
+	}
+}
+
+class Link extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { name: this.props.link.name, url: this.props.link.url };
+	}
+	cleanupURL(url) {
+		url = url.replace(/(.*?:\/\/)|(www\.)/g, '').replace(/\/.*/, '');
+		return url;
+	}
+	render() {
+		return (
+			<div className='link'>
+				<h1 className='linkName'>{this.state.name}</h1>
+				{/* <h2 className='linkURL'>{this.cleanupURL(this.state.url)}</h2> */}
+			</div>
+		);
+	}
+}
+
+// attaching our react system to the div of id='root' in ~/public/index.html
+ReactDOM.render(<Main userJSON={example} />, document.getElementById('root'));
+// optimises render workflow (caching to device)
 serviceWorker.register();
