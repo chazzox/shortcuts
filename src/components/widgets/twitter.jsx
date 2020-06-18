73b00ef46299ca6733b0af7f5bead5b39a828524
chazzox <42206116+chazzox@users.noreply.github.com>
2020-06-18 23:45:49 +0100

    upgrading the api route to htttps

--- src/components/widgets/twitter.jsx
+++ src/components/widgets/twitter.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Cookies from 'js-cookie';
 
 const callbackRegexTwitter = /^oauth_token=([\w-]*)&oauth_verifier=([\w-]*)$/;
-const apiURL = 'http://ec2-52-91-0-119.compute-1.amazonaws.com:8000';
+const apiURL = 'https://ec2-52-91-0-119.compute-1.amazonaws.com:8000';
 
 export default class Twitter extends React.Component {
 	constructor(props) {
