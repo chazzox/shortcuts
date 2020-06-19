4a1dbf65c0e3a9145945870d1721d28501eab76e
charlie <42206116+chazzox@users.noreply.github.com>
2020-06-19 01:27:56 +0100

    https support

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const express = require('express');
+cosnt fs = require('fs');
 const bodyParser = require('body-parser');
+const http = require('http');
+const https = require('https');
+const express = require('express');
 const app = express();
 
 const twitterAPI = require('./api/twitter');
-const port = 8000;
-
-// Body parser middleware
-
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-	bodyParser.urlencoded({
-		extended: false
-	});
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
+        bodyParser.urlencoded({
+                extended: false
+        });
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();
 });
 
 app.use('/twitter', twitterAPI);
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer({
+  key: fs.readFileSync('./privatekey.pem'),
+  cert: fs.readFileSync('./server.crt')
+}, app);
+
+httpServer.listen(8080);
+httpsServer.listen(443);
