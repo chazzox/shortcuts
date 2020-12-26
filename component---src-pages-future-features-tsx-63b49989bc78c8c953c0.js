68114cc677bdda483903d5801fe58e45e0d9304f
chazzox <chazzox@users.noreply.github.com>
2020-12-26 00:11:57 +0000

    deploy: 43fcc4514628acfdb7b6f434d6d261abbfa7c506

---
+++ component---src-pages-future-features-tsx-63b49989bc78c8c953c0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"At+a":function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),u=t.n(a);e.default=function(){return u.a.createElement("h1",null,"new features")}}}]);
+//# sourceMappingURL=component---src-pages-future-features-tsx-63b49989bc78c8c953c0.js.map
