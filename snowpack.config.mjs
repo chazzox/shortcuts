26d3a26dfc497b5116a8774095da220c9858f59d
chazzox <42206116+chazzox@users.noreply.github.com>
2021-12-18 22:45:12 +0000

    configs

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -7,28 +7,12 @@ export default {
 	plugins: [
 		'@snowpack/plugin-react-refresh',
 		'@snowpack/plugin-dotenv',
-		[
-			'@snowpack/plugin-typescript',
-			{
-				/* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
-				...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {})
-			}
-		]
+		'@snowpack/plugin-typescript'
 	],
-	routes: [
-		/* Enable an SPA Fallback in development: */
-		// {"match": "routes", "src": ".*", "dest": "/index.html"},
-	],
-	optimize: {
-		/* Example: Bundle your final build: */
-		// "bundle": true,
-	},
-	packageOptions: {
-		/* ... */
-	},
-	devOptions: {
-		/* ... */
-	},
+	routes: [],
+	optimize: {},
+	packageOptions: {},
+	devOptions: {},
 	buildOptions: {
 		baseUrl: '/shortcuts'
 	},
