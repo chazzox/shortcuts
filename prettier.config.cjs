a2bfe6c327727924f1b6dfd6dda47ff69ff6b8f5
chazzox <caylott@outlook.com>
2023-05-09 08:23:10 +0100

    dynamic grid

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/**
+ * @type {import('@types/prettier').Options}
+ */
+module.exports = {
+  useTabs: false,
+  tabWidth: 4,
+  singleQuote: false,
+  trailingComma: "none",
+  proseWrap: "always",
+  printWidth: 85,
+  plugins: []
+};
